4c70554050ca77955eb9e722e45238de3fac7f12
Gabriel <gmonteeiro@gmail.com>
2018-09-17 13:33:40 -0300

    novo projeto e

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,336 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.drifty.cordova.cameraroll.CameraRoll",
+        "file": "plugins/com.drifty.cordova.cameraroll/www/CameraRoll.js",
+        "pluginId": "com.drifty.cordova.cameraroll",
+        "clobbers": [
+            "CameraRoll"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.Camera",
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.camera",
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.CameraPopoverHandle",
+        "file": "plugins/cordova-plugin-camera/www/ios/CameraPopoverHandle.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.console",
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.logger",
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.DirectoryEntry",
+        "file": "plugins/cordova-plugin-file/www/DirectoryEntry.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.DirectoryEntry"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.DirectoryReader",
+        "file": "plugins/cordova-plugin-file/www/DirectoryReader.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.DirectoryReader"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.Entry",
+        "file": "plugins/cordova-plugin-file/www/Entry.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.Entry"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.File",
+        "file": "plugins/cordova-plugin-file/www/File.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.File"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.FileEntry",
+        "file": "plugins/cordova-plugin-file/www/FileEntry.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.FileEntry"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.FileError",
+        "file": "plugins/cordova-plugin-file/www/FileError.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.FileError"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.FileReader",
+        "file": "plugins/cordova-plugin-file/www/FileReader.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.FileReader"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.FileSystem",
+        "file": "plugins/cordova-plugin-file/www/FileSystem.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.FileSystem"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.FileUploadOptions",
+        "file": "plugins/cordova-plugin-file/www/FileUploadOptions.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.FileUploadOptions"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.FileUploadResult",
+        "file": "plugins/cordova-plugin-file/www/FileUploadResult.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.FileUploadResult"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.FileWriter",
+        "file": "plugins/cordova-plugin-file/www/FileWriter.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.FileWriter"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.Flags",
+        "file": "plugins/cordova-plugin-file/www/Flags.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.Flags"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.LocalFileSystem",
+        "file": "plugins/cordova-plugin-file/www/LocalFileSystem.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.LocalFileSystem"
+        ],
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.Metadata",
+        "file": "plugins/cordova-plugin-file/www/Metadata.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.Metadata"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.ProgressEvent",
+        "file": "plugins/cordova-plugin-file/www/ProgressEvent.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.ProgressEvent"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.fileSystems",
+        "file": "plugins/cordova-plugin-file/www/fileSystems.js",
+        "pluginId": "cordova-plugin-file"
+    },
+    {
+        "id": "cordova-plugin-file.requestFileSystem",
+        "file": "plugins/cordova-plugin-file/www/requestFileSystem.js",
+        "pluginId": "cordova-plugin-file",
+        "clobbers": [
+            "window.requestFileSystem"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.resolveLocalFileSystemURI",
+        "file": "plugins/cordova-plugin-file/www/resolveLocalFileSystemURI.js",
+        "pluginId": "cordova-plugin-file",
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.isChrome",
+        "file": "plugins/cordova-plugin-file/www/browser/isChrome.js",
+        "pluginId": "cordova-plugin-file",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-file.iosFileSystem",
+        "file": "plugins/cordova-plugin-file/www/ios/FileSystem.js",
+        "pluginId": "cordova-plugin-file",
+        "merges": [
+            "FileSystem"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file.fileSystems-roots",
+        "file": "plugins/cordova-plugin-file/www/fileSystems-roots.js",
+        "pluginId": "cordova-plugin-file",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-file.fileSystemPaths",
+        "file": "plugins/cordova-plugin-file/www/fileSystemPaths.js",
+        "pluginId": "cordova-plugin-file",
+        "merges": [
+            "cordova"
+        ],
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "id": "cordova-plugin-ionic-keyboard.keyboard",
+        "file": "plugins/cordova-plugin-ionic-keyboard/www/ios/keyboard.js",
+        "pluginId": "cordova-plugin-ionic-keyboard",
+        "clobbers": [
+            "window.Keyboard"
+        ]
+    },
+    {
+        "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+        "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+        "pluginId": "cordova-plugin-ionic-webview",
+        "clobbers": [
+            "cordova.exec"
+        ]
+    },
+    {
+        "id": "cordova-plugin-photokandy-video-thumbnail.PKVideoThumbnail",
+        "file": "plugins/cordova-plugin-photokandy-video-thumbnail/www/PKVideoThumbnail.js",
+        "pluginId": "cordova-plugin-photokandy-video-thumbnail",
+        "clobbers": [
+            "window.PKVideoThumbnail"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "cordova-plugin-streaming-media.StreamingMedia",
+        "file": "plugins/cordova-plugin-streaming-media/www/StreamingMedia.js",
+        "pluginId": "cordova-plugin-streaming-media",
+        "clobbers": [
+            "streamingMedia"
+        ]
+    },
+    {
+        "id": "cordova-plugin-video-thumbnail.video-thumbnail",
+        "file": "plugins/cordova-plugin-video-thumbnail/www/VideoThumbnail.js",
+        "pluginId": "cordova-plugin-video-thumbnail",
+        "clobbers": [
+            "navigator.createThumbnail"
+        ]
+    },
+    {
+        "id": "ionic-plugin-keyboard.keyboard",
+        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.drifty.cordova.cameraroll": "0.2.1",
+    "cordova-plugin-compat": "1.2.0",
+    "cordova-plugin-camera": "2.4.1",
+    "cordova-plugin-console": "1.0.5",
+    "cordova-plugin-device": "1.1.4",
+    "cordova-plugin-file": "4.3.3",
+    "cordova-plugin-inappbrowser": "1.7.1",
+    "cordova-plugin-ionic-keyboard": "2.0.5",
+    "cordova-plugin-ionic-webview": "1.1.16",
+    "cordova-plugin-photokandy-video-thumbnail": "2.1.0",
+    "cordova-plugin-splashscreen": "4.0.3",
+    "cordova-plugin-statusbar": "2.2.1",
+    "cordova-plugin-streaming-media": "2.1.3",
+    "cordova-plugin-video-thumbnail": "2.0.1",
+    "cordova-plugin-whitelist": "1.3.1",
+    "ionic-plugin-keyboard": "2.2.1"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,451 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $timeout, $ionicPlatform, $ionicConfig, localService, $ionicHistory, $state, $ionicPopup) {
+            
+            $ionicPlatform.ready(function() {
+                                 $ionicConfig.backButton.text("");
+                                 
+                                 $scope.goHome = function(){
+                                 $ionicHistory.nextViewOptions({
+                                                               disableBack: true
+                                                               });
+                                 $state.go('app.home');
+                                 }
+                                 });
+            
+            $scope.myTitle = '<img src="img/logohoteis_shell.png">';
+            
+            $scope.showAlert = function(opts) {
+            var alertPopup = $ionicPopup.alert({
+                                               title: opts.title,
+                                               template: opts.template
+                                               });
+            alertPopup.then(opts.fn());
+            };
+            })
+
+.controller('HomeCtrl', function($scope, $rootScope, $state, $ionicHistory, localService, $ionicModal) {
+            $scope.go = function(){
+            $ionicHistory.clearCache();
+            $ionicHistory.clearHistory();
+            $state.go('app.single');
+            }
+            })
+
+.controller('QuestoesCtrl', function($scope, $stateParams, $state, $ionicHistory, $ionicConfig, localService, $q, apiService, $ionicPopup, $sce) {
+  $ionicConfig.backButton.text("");
+  $ionicHistory.clearCache();
+  // $ionicHistory.clearHistory();
+  
+  $scope.accept = true;
+  
+  $scope.cadastro = {
+    "visitantes" : [],
+    "imagem" : null,
+    "video" : null
+  }
+  $scope.visitante = {};
+  
+  $scope.showOpt = false;
+  var npic = 0; var n = 0; var k = -1;
+  var foto = null;
+            
+  function verify(){
+    setTimeout(function() {
+      console.log(n);
+      if(k < n){ 
+        k = n; verify(); 
+      }else{ 
+        resolve(foto); 
+         // $scope.cadastro.imagem = foto;
+         // $scope.$apply();
+      }
+    }, 500);
+  }
+
+  trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  }
+  
+  var resolve = function(url){
+    console.log("resolve");
+    console.log(url);
+    if(url){
+      resolveLocalFileSystemURL(url, function(fileEntry) {
+        fileEntry.file(function(file) {
+          var reader = new FileReader();
+          reader.onloadend = function(event) {
+            console.log(event.target.result.byteLength);
+          };
+
+          reader.readAsDataURL(file);
+          console.log(file);
+
+          var videoUrl = url;
+
+          // Just play a video
+          //window.plugins.streamingMedia.playVideo(videoUrl);
+          var opt = {
+            mode : 'base64'
+          }
+
+          window.PKVideoThumbnail.createThumbnail (videoUrl, "ignore", opt)
+          .then(function(success){
+            console.log("success");
+            $scope.cadastro.imagem = success;
+            $scope.$apply();
+          })
+          .catch(function(err){
+            console.log("err");
+            console.log(err);
+          });
+
+          $scope.playVd = function(){
+            window.plugins.streamingMedia.playVideo(videoUrl, options);
+          }
+
+          // Play a video with callbacks
+          var options = {
+            successCallback: function() {
+              console.log("Video was closed without error.");
+            },
+            errorCallback: function(errMsg) {
+              console.log("Error! " + errMsg);
+            },
+            orientation: 'portrait',
+            shouldAutoClose: true,  // true(default)/false
+            controls: true // true(default)/false. Used to hide controls on fullscreen
+          };
+
+          // $scope.cadastro.imagem = trustSrc(url);
+          $scope.cadastro.imgUrl = file.localURL;
+          console.log('atribuiu');
+          $scope.cadastro.video = file.localURL;
+          
+        });
+      });
+    }
+  }
+            
+  CameraRoll.getPhotos(function(photo) {
+    if(k < 0){verify(); k=0;}
+    if(photo){
+      n++;
+      foto = photo;
+    }
+  });
+            
+  function addV(){
+    $scope.cadastro.visitantes.push({
+      "nome" : $scope.visitante.nome,
+      "mail" : $scope.visitante.mail,
+      "fone" : $scope.visitante.fone,
+      "sobrenome" : $scope.visitante.sobrenome
+    });
+    $scope.visitante = {};
+  }
+            
+  $scope.addVisitante = function(){
+    validaVisitante().then(function(success){
+      addV();
+    }, function(err){
+      console.log(err);
+      $scope.showAlert({ title: err.msg, fn: function(){} });
+    });
+  }
+            
+  $scope.save = function(sync){
+    validaVisitante().then(function(){
+      addV();
+      env();
+    }, function(err){
+      if(err.cod == 1){
+        if($scope.cadastro.visitantes.length > 0){
+          env();
+        }else{
+          $scope.showAlert({ title: err.msg, fn: function(){} });
+        }
+      }else{
+        $scope.showAlert({ title: err.msg, fn: function(){} });
+      }
+    });
+            
+    function env(){
+      validaEnvio().then(function(success){
+        if(sync){
+          adSync($scope.cadastro);
+        }else{
+          console.log($scope.cadastro);
+          localService.setCadastro($scope.cadastro);
+          $ionicHistory.nextViewOptions({disableBack: true});
+          $state.go('app.success');
+        }
+      }, function(err){
+        $scope.showAlert({ title: err, fn: function(){} });
+      });
+    }
+  }
+
+  $scope.cancel = function(){
+    $ionicHistory.nextViewOptions({ disableBack: true });
+    $state.go('app.home');
+    $scope.synced = true;
+  }
+            
+  function adSync(data){
+    var sync = localService.getSync();
+    delete data.imagem;
+    (sync.dados) ? sync.dados.push(data) : sync.dados = [data];
+    localService.setSync(sync);
+    
+    var alertPp = $ionicPopup.alert({ title: "Dados adicionados com Sucesso!" });
+    alertPp.then(function(){
+      $ionicHistory.nextViewOptions({disableBack: true});
+      $state.go('app.home');
+    });
+  }
+            
+  $scope.toggleTermos = function(){
+    $scope.accept = ($scope.accept) ? false : true;
+  }
+            
+  function validaVisitante() {
+    return $q(function(resolve, reject) {
+      // var rgxCpf = new RegExp("[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}");
+      var rgxFone = /^\([1-9]{2}\) [2-9][0-9]{3,4}\-[0-9]{4}?$/i;
+      // var rgxMail = /^[a-zA-Z 0-9]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+      var rgxMail = /^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?$/i;
+
+      if($scope.visitante.nome && $scope.visitante.mail && $scope.visitante.fone){
+        // (!rgxCpf.test($scope.visitante.cpf)) ? reject({"cod": 2, "msg": "CPF Inválido"}) : null;
+        (!rgxFone.test($scope.visitante.fone)) ? reject({"cod": 2, "msg": "Fone Inválido"}) : null;
+        (!rgxMail.test($scope.visitante.mail)) ? reject({"cod": 2, "msg": "E-mail Inválido"}) : null;
+        resolve("sucesso");
+      }else{
+        reject({"cod": 1, "msg": "Preencha todos os campos!"});
+      }
+    });
+  }
+            
+  function validaEnvio() {
+    return $q(function(resolve, reject) {
+      (!$scope.accept) ? reject("Para participar dessa ação, você precisa autorizar Hoteis.com!") : null;
+      resolve("sucesso");
+    });
+  }
+    
+  $scope.foneMask = function(){
+    if($scope.visitante.fone){
+      // d = $scope.visitante.cpf.replace(/\D/g,"");
+      // d=d.replace(/(\d{3})(\d)/,"$1.$2");
+      // d=d.replace(/(\d{3})(\d)/,"$1.$2");
+      // d=d.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+      // $scope.visitante.cpf = d;
+      
+      v=$scope.visitante.fone.replace(/\D/g,"");
+      v=v.substring(0, 11);
+      v=v.replace(/^(\d{2})(\d)/g,"($1) $2");
+      v=v.replace(/(\d)(\d{4})$/,"$1-$2");
+      $scope.visitante.fone = v;
+    }
+  }
+})
+
+.controller('SuccessCtrl', function($scope, $rootScope, $ionicHistory, $state, localService, $ionicPopup, $ionicConfig, apiService) {
+  var data = localService.getCadastro();
+
+  apiService.getFileContentAsBase64(data.imgUrl,function(base64){
+    data.video = base64;
+    $scope.send();
+  });
+
+  $scope.send = function(){
+    $scope.sent = false;
+    $scope.done = false;
+    $scope.erro = false;
+
+    apiService.postLocal(data, function (res) {
+      $scope.sent = true;
+      setTimeout(function() { $scope.done = true; }, 500);
+    },function(err){
+      console.log(err);
+      $scope.sent = true;
+      setTimeout(function() { $scope.erro = true; }, 500);
+      //$scope.showAlert({ title: "Não foi possível enviar seus dados!", fn: function(){} });
+    });
+  }
+
+  $scope.save = function(){
+    $ionicHistory.nextViewOptions({ disableBack: true });
+    $state.go('app.home');
+  }
+
+  $scope.addSync = function(){
+    var sync = localService.getSync();
+    delete data.video;
+    delete data.imagem;
+    (sync.dados) ? sync.dados.push(data) : sync.dados = [data];
+    localService.setSync(sync);
+    $scope.save();
+  }
+})
+
+.controller('SyncCtrl', function($scope, apiService, $ionicLoading, localService, $ionicHistory, $state, $ionicPopup){
+  var senha = 123456;
+  $scope.synced = false;
+  $scope.logar = {};
+  var x = 0; var wdt = 0;
+  var el = null;
+  
+  $scope.showSync = false;
+  $scope.doSync = function(){
+    console.log("chamou");
+    if($scope.logar.senha == senha){
+      $scope.showSync = true;
+      var dt = localService.getSync();
+      (!dt.dados) ? dt = {"dados":[]} : null;
+      console.log("sync");
+      console.log(dt.dados);
+      x = 100/dt.dados.length;
+      el = document.getElementById("pgrs");
+      console.log(el);
+      sincronizar(dt.dados);
+    }else{
+      $scope.showAlert({
+        title: 'Senha Inválida',
+        template: 'tente novamente.',
+        fn: function(){
+          $scope.logar.senha = null;
+        }
+      });
+    }
+  }
+  
+  sincronizar = function(dados){
+    if(dados && dados.length > 0){
+      var sendData = dados.pop();
+      console.log(sendData);
+      if(sendData.imgUrl){
+        apiService.getFileContentAsBase64(sendData.imgUrl,function(base64Image){
+          sendData.video = base64Image;
+      
+          setTimeout(function() {
+            console.log("sincronizar");
+            console.log(sendData);
+            apiService.postLocal(sendData, function (res) {
+              console.log(res);
+              setTimeout(function() {
+                wdt = wdt+x;
+                el.style.width = wdt + '%';
+                sincronizar(dados);
+              }, 300);
+            }, function(res){
+              console.log(res);
+              delete sendData.video;
+              delete sendData.imagem;
+              dados.push(sendData);
+              localService.setSync({"dados" : dados});
+              setTimeout(function() {
+                $scope.erro = true;
+                $scope.$apply();
+              }, 500);
+            });
+          }, 200);
+        });
+      }else{
+        wdt = wdt+x;
+        el.style.width = wdt + '%';
+        sincronizar(dados);
+      }
+    }else{
+      if(el){
+        el.style.width = '0.1%';
+      }
+      console.log("fim sincronizar");
+      localService.setSync({"dados" : dados});
+      setTimeout(function() {
+        $scope.synced = true;
+        $scope.$apply();
+      }, 900);
+    }
+  }
+  
+  $scope.cancel = function(){
+    $ionicHistory.nextViewOptions({ disableBack: true });
+    $state.go('app.home');
+    $scope.synced = true;
+  }
+})
+
+.service('apiService', function($http){
+         
+  var post = function(data, success, failure){
+    return $http.post('https://qualitydigitalserver2.com.br/ServiceHoteisPontoCom/api/values', data)
+    .then(function (result) {
+      success(result);
+    }, function (error) {
+      failure(error);
+    });
+  }
+  //http://192.168.0.104/Api/Values
+  var postLocal = function(data, success, failure){
+    return $http.post('http://192.168.0.104/Api/Values', data)
+    .then(function (result) {
+      success(result);
+    }, function (error) {
+      failure(error);
+    });
+  }
+
+  var getFileContentAsBase64 = function(path,callback){
+    window.resolveLocalFileSystemURL(path, gotFile, fail);
+
+    function fail(e) {
+      console.log('Cannot found requested file');
+    }
+
+    function gotFile(fileEntry) {
+      fileEntry.file(function(file) {
+        var reader = new FileReader();
+        reader.onloadend = function(e) {
+          var content = this.result;
+          callback(content);
+        };
+        reader.readAsDataURL(file);
+      });
+    }
+  }
+
+  return {
+    post:post,
+    postLocal:postLocal,
+    getFileContentAsBase64:getFileContentAsBase64
+  };
+})
+
+.service('localService', function($window){
+         var setCadastro = function(data) {
+         window.localStorage.cadastro = JSON.stringify(data);
+         };
+         
+         var getCadastro = function(){
+         return JSON.parse(window.localStorage.cadastro || '{}');
+         };
+         
+         var setSync = function(data) {
+         window.localStorage.sync = JSON.stringify(data);
+         };
+         
+         var getSync = function(){
+         return JSON.parse(window.localStorage.sync || '{}');
+         };
+         
+         return {
+         getCadastro     : getCadastro,
+         setCadastro     : setCadastro,
+         getSync         : getSync,
+         setSync         : setSync
+         };
+         
+         });
+
